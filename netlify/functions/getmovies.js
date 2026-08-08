aa6ecbe5075638fb511e0b2be4e60d2d49b0adac
Daudui <Daud59302@gmail.com>
2026-08-08 18:02:16 +0200

    changes

--- netlify/functions/getmovies.js
+++ netlify/functions/getmovies.js
@@ -1,35 +1,36 @@
 // netlify/functions/getmovies.js
 exports.handler = async function (event, context) {
-  try {
-    let endpoint = event.queryStringParameters.endpoint || "movie/popular";
-
-    // Vi bruker process.env som fungerer i alle Netlify-versjoner
-    const apiKey = process.env.PRIVATE_API_KEY;
-
-    if (!apiKey) {
+    try {
+      let endpoint = event.queryStringParameters.endpoint || "movie/popular";
+  
+      // Vi bruker process.env som fungerer i alle Netlify-versjoner
+      const apiKey = process.env.PRIVATE_API_KEY;
+  
+      if (!apiKey) {
+        return {
+          statusCode: 500,
+          body: JSON.stringify({
+            error: "PRIVATE_API_KEY mangler i Netlify-dashboardet!",
+          }),
+        };
+      }
+  
+      endpoint = endpoint.replace(/^\/+/, "");
+      const url = `https://themoviedb.org{endpoint}?api_key=${apiKey}`;
+  
+      const response = await fetch(url);
+      const data = await response.json();
+  
       return {
-        statusCode: 500,
-        body: JSON.stringify({
-          error: "PRIVATE_API_KEY mangler i Netlify-dashboardet!",
-        }),
+        statusCode: 200,
+        headers: {
+          "Content-Type": "application/json",
+          "Access-Control-Allow-Origin": "*",
+        },
+        body: JSON.stringify(data),
       };
+    } catch (error) {
+      return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
     }
-
-    endpoint = endpoint.replace(/^\/+/, "");
-    const url = `https://themoviedb.org{endpoint}?api_key=${apiKey}`;
-
-    const response = await fetch(url);
-    const data = await response.json();
-
-    return {
-      statusCode: 200,
-      headers: {
-        "Content-Type": "application/json",
-        "Access-Control-Allow-Origin": "*",
-      },
-      body: JSON.stringify(data),
-    };
-  } catch (error) {
-    return { statusCode: 500, body: JSON.stringify({ error: error.message }) };
-  }
-};
+  };
+  
